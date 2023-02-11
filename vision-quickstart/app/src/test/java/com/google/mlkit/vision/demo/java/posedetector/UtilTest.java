6e59d0c5795cbbe5e0ec7a8fd032946da73c4b65
Sheldon Chen <nh8157@hotmail.com>
2023-02-11 11:52:42 +0000

    Added unit tests for Util class.

--- vision-quickstart/app/src/test/java/com/google/mlkit/vision/demo/java/posedetector/UtilTest.java
+++ vision-quickstart/app/src/test/java/com/google/mlkit/vision/demo/java/posedetector/UtilTest.java
@@ -5,13 +5,23 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class UtilTest {
+    private final double delta = 0.1;
 
     @Test
-    public void getDistance() {
-
+    public void testGetDistance() {
+        assertEquals("Testing getDistance 1", Util.getDistance(0.0, 0.0, 10.0, 0.0), 10.0, delta);
+        assertEquals("Testing getDistance 2", Util.getDistance(0.0, 10.0, 0.0, 0.0), 10.0, delta);
+        assertEquals("Testing getDistance 3", Util.getDistance(0.0, 0.0, 10.0, 10.0), 14.14, delta);
+        assertEquals("Testing getDistance 4", Util.getDistance(10.0, 10.0, 0.0, 0.0), 14.14, delta);
+        assertEquals("Testing getDistance 5", Util.getDistance(1.732, 1.0, 0.0, 0.0), 2.0, delta);
     }
 
     @Test
-    public void getAngle() {
+    public void testGetAngle() {
+        assertEquals("Testing getAngle 1", Util.getAngle(0.0, 10.0, 0.0, 0.0, 10.0, 0.0), 90.0, delta);
+        assertEquals("Testing getAngle 2", Util.getAngle(0.0, 10.0, 0.0, 0.0, 0.0, -10.0), 180.0, delta);
+        assertEquals("Testing getAngle 3", Util.getAngle(10.0, 10.0, 0.0, 0.0, 10.0, 0.0), 45.0, delta);
+        assertEquals("Testing getAngle 4", Util.getAngle(10.0, 10.0, 0.0, 0.0, 10.0, 0.0), 45.0, delta);
+        assertEquals("Testing getAngle 5", Util.getAngle(1.0, 1.732, 0.0, 0.0, 1.0, 0.0), 60.0, delta);
     }
 }
