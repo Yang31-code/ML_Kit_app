bfa921f478917e859263875683e94348df54913c
Sheldon Chen <nh8157@hotmail.com>
2023-02-11 11:28:00 +0000

    Added unit test folder.

---
+++ vision-quickstart/app/src/test/java/com/google/mlkit/vision/demo/java/posedetector/UtilTest.java
@@ -0,0 +1,17 @@
+package com.google.mlkit.vision.demo.java.posedetector;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UtilTest {
+
+    @Test
+    public void getDistance() {
+
+    }
+
+    @Test
+    public void getAngle() {
+    }
+}
