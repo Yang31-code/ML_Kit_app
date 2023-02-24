8554bed07d03de64a323ed18dc0b40039cd5bd3e
Kallum Panditharatna <kallum179@gmail.com>
2023-02-24 07:47:52 +0000

    :sparkles: Point above point implemented without testing

--- vision-quickstart/app/src/main/java/com/google/mlkit/vision/demo/java/posedetector/Keyframe.java
+++ vision-quickstart/app/src/main/java/com/google/mlkit/vision/demo/java/posedetector/Keyframe.java
@@ -132,6 +132,66 @@ class UniPointPosition implements Point {
     }
 }
 
+class PointAbovePoint implements Point {
+
+    private List<Integer> toTrack;
+
+    private Double actualBelow;
+    private Double actualAbove;
+    
+    private double leniency;
+
+
+    public PointAbovePoint(JSONObject json) {
+
+        try {
+
+            //to track parsing
+            JSONArray toTrackJson = (JSONArray) json.get("toTrack"); //should only have two points
+            toTrack.add((int) toTrackJson.get(0)); //adds the point to be below
+            toTrack.add((int) toTrackJson.get(1)); //adds the second to be above
+
+            //leniency parsing
+            leniency = Double.parseDouble(json.get("leniency").toString());
+
+
+        } catch  (JSONException e) {
+
+            System.out.println(e);
+            return;
+
+        }
+    }
+
+
+    @Override
+    public boolean isValidPoint(List<List<Double>> landmarks) {
+
+        List<Double> toBeBelowVec2 = landmarks.get(toTrack.get(0));
+        List<Double> toBeAboveVec2 = landmarks.get(toTrack.get(1));
+
+        double toBeBelowY = toBeBelowVec2.get(0);
+        double toBeAboveY = toBeAboveVec2.get(0);
+
+        actualBelow = toBeBelowY;
+        actualAbove = toBeAboveY;
+
+
+        return toBeAboveY - leniency > toBeBelowY;
+    }
+
+    @Override
+    public List<String>  getInfo() {
+
+        List<String> poseFeedback = new ArrayList<>();
+
+        poseFeedback.add("Point that should be lower's y: " + String.format("%.1f", actualBelow));
+        poseFeedback.add("Point that should be above's y: " + String.format("%.1f", actualAbove));
+
+        return poseFeedback;
+    }
+}
+
 public class Keyframe implements Point {
     private List<Point> points;
     private Instant startTime;
